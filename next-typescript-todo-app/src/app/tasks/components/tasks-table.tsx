ee870d95fb3581e025f200d49cee6d91a1407236
peterborisov <borisov.peter@yahoo.co.uk>
2024-06-20 10:38:44 +0300

    table pagination

--- next-typescript-todo-app/src/app/tasks/components/tasks-table.tsx
+++ next-typescript-todo-app/src/app/tasks/components/tasks-table.tsx
@@ -1,13 +1,14 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { useRouter } from "next/navigation";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import TablePagination from "@mui/material/TablePagination";
 import { Task } from "@task-types/task-types";
-import Button from "@mui/material/Button";
-import { useDispatch, useSelector } from "react-redux";
-import { useRouter } from "next/navigation";
 
 type TasksTableProps = {
   rows: Task[];
@@ -20,35 +21,63 @@ export const TasksTable: FC<TasksTableProps> = ({ rows }) => {
     router.push(`/tasks/${task.id}`);
   };
 
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   return (
-    <Table sx={{ maxWidth: 650 }}>
-      <TableHead>
-        <TableRow>
-          <TableCell>userId</TableCell>
-          <TableCell align="right">ID</TableCell>
-          <TableCell align="right">Title</TableCell>
-          <TableCell align="right">Completed</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {rows.map((task: Task) => (
-          <TableRow key={task.id}>
-            <TableCell>{task.userId}</TableCell>
-            <TableCell align="right">{task.id}</TableCell>
-            <TableCell align="right">{task.title}</TableCell>
-            <TableCell align="right">
-              <Button
-                variant="outlined"
-                color="success"
-                size="small"
-                onClick={() => taskDetails(task)}
-              >
-                Task Details
-              </Button>
-            </TableCell>
+    <Paper>
+      <Table sx={{ maxWidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>userId</TableCell>
+            <TableCell align="right">ID</TableCell>
+            <TableCell align="right">Title</TableCell>
+            <TableCell align="right">Completed</TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHead>
+        <TableBody>
+          {rows
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((task: Task) => (
+              <TableRow key={task.id}>
+                <TableCell>{task.userId}</TableCell>
+                <TableCell align="right">{task.id}</TableCell>
+                <TableCell align="right">{task.title}</TableCell>
+                <TableCell align="right">
+                  <Button
+                    variant="outlined"
+                    color="success"
+                    size="small"
+                    onClick={() => taskDetails(task)}
+                  >
+                    Task Details
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+      <TablePagination
+        sx={{ maxWidth: 650 }}
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
   );
 };
